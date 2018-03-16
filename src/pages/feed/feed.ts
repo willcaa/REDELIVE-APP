c6f014d3bb96e163acce1b4897b2a392cafe42e9
Victor Hugo <victor_neo99@hotmail.com>
2018-03-16 17:06:38 -0300

    final ?

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -150,6 +150,27 @@ export class FeedPage {
     }, 2000);
   }
 
+  seguidores() {
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Accept', 'application/json');
+    headers.append('content-type', 'application/json');
+    headers.append('Access-Control-Expose-Headers', "true");
+
+    let body = {
+      id_usuario: this.userId
+    }
+    var link = 'https://bluedropsproducts.com/app/anuncios/seguidores';
+
+    this.http.post(link, JSON.stringify(body), { headers: headers })
+      .map(res => res.json())
+      .subscribe(data => {
+        this.index_feed = 0;
+        this.feed = data;
+        console.log(data);
+      });
+  }
+
   alterarLocal(newLocal) {
     this.local = newLocal;
     this.index_feed = 0;
@@ -314,7 +335,6 @@ export class FeedPage {
     //     break;
     //   }
     // }
-   
     if(this.local == "bairro"){
       localNome = this.bairro;
     }
