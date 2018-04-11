70cd326b2aad56c49b33fd4d7f22d10ccfaea356
Victor Hugo <victor_neo99@hotmail.com>
2018-04-11 16:41:43 -0300

    likes em forma de estrelas

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -70,6 +70,28 @@ export class FeedPage {
     }
   }
 
+  enviarEstrelas(stars, anuncio) {
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Accept', 'application/json');
+    headers.append('content-type', 'application/json');
+    headers.append('Access-Control-Expose-Headers', "true");
+
+    let body = {
+      id_usuario: this.userId,
+      id_anuncio: anuncio,
+      n_estrelas: stars
+    }
+
+    var link = 'https://bluedropsproducts.com/app/anuncios/stars';
+
+    this.http.post(link, JSON.stringify(body), { headers: headers })
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
   denunciarPost(post) {
     if(post.id_anuncio == this.userId) {
       this.showAlert("OPA!","Você não pode denunciar o proprio anuncio!","OK");
@@ -123,7 +145,7 @@ export class FeedPage {
     });
 
     popover.onDidDismiss(popoverData => {
-      if(popoverData != this.topOrNews) {
+      if(popoverData != this.topOrNews && (popoverData == "Top" || popoverData == "News")) {
         this.topOrNews = popoverData;
         this.index_feed = 0;
         this.feed = [];
